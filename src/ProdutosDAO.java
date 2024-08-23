98f93f3895981c8b5a95140afffea3d4076ff652
Gabriel S. Veiga <gabrielsveiga2507@gmail.com>
2024-08-23 15:37:13 -0300

    Implementação do botão e da funcionalidade de cadastro de produtos no BC

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -8,6 +8,7 @@
  * @author Adm
  */
 
+import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
@@ -23,11 +24,27 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+        try {
+        conn = new conectaDAO().connectDB();
         
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES(?,?,?);";
         
-        //conn = new conectaDAO().connectDB();
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
         
+        prep.execute();
         
+        if(conn != null && !conn.isClosed()) {
+            conn.close();
+            System.out.println("DESCONECTADO DA DB!");
+        }
+        
+        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERRO ao cadastrar o produto no banco de dados");
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,30 +4,40 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
 public class conectaDAO {
-    
-    public Connection connectDB(){
-        Connection conn = null;
-        
+
+    Connection conn = null;
+
+    public Connection connectDB() {
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11", "root", "GSV052507root");
+            System.out.println("SUCESSO DE CONEXAO!");
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "FALHA AO TENTAR CARREGAR CLASSE DE CONEXAO!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "FALHA AO TENTAR CONECTAR COM O DB, ERRO SQL!");
         }
+
         return conn;
     }
-    
+
+    public void desconectDB() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+                System.out.println("DESCONECTADO DA DB!");
+            }
+        } catch (SQLException ex) {
+            System.out.println("FALHA AO TENTAR DESCONECTAR COM O DB, ERRO SQL!");
+        }
+    }
 }
